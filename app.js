cfb0c0d18036c149c4059d3cd58d879b2c6517b9
omidn <o.najaee@gmail.com>
2016-08-20 18:17:41 +0200

    bug fixed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto');
 const config = require('./config');
+const request = require('request');
 
 const hmac = crypto.createHmac('sha256', config.SECRET_KEY);
 
@@ -11,19 +12,32 @@ var fixDate = function(date) {
 
 let protocol = 'https://';
 let endpoint = 'api.trivago.com';
-let path = '/webservice/tas/hotels';
+let path = '/webservice/tas/locations';
 
-let endDate = new Date();
-endDate.setDate(endDate.getDate() + 7);
+// let endDate = new Date();
+// endDate.setDate(endDate.getDate() + 7);
+console.log(config.ACCESS_ID);
 let query = {
 	access_id: config.ACCESS_ID,
-	end_date: endDate,
-	start_date: new Date().toISOString(),
+	// end_date: endDate,
+	// start_date: new Date().toISOString(),
+	query: 'berlin',
 	timestamp: fixDate(new Date())
 };
 
 query = Object.keys(query).sort().map((key) => key + '=' + encodeURIComponent(query[key])).join('&');
+console.log(query);
 
-var unhashed_signature = ['GET', endpoint, path, query].join('\r');
+var unhashed_signature = ['GET', endpoint, path, query].join('\n');
 query += '&signature=' + hmac.update(unhashed_signature).digest('base64');
-console.log(protocol + endpoint + path + '?' + query);
+let url = protocol + endpoint + path + '?' + query;
+
+request(url, {
+	headers: {
+		'Accept' :'application/vnd.trivago.affiliate.hal+json;version=1',
+		'Accept-Language': 'en-GB'
+	}
+}, function(err, resp) {
+	console.log(err, resp.body);
+
+});
